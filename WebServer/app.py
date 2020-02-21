e7911460587553fee31af8055e8ab8e016055555
Lorenzo <lorenzo.adam.piazza@protonmail.ch>
2020-02-21 22:38:30 +0100

    Aggiunto codice per interagire con database

--- WebServer/app.py
+++ WebServer/app.py
@@ -1,7 +1,36 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("classroom_ar_scanner")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://query:Punk42h2AsaCbzs@db:3306/classroom_timetables"
+app.config["SQLALCHEMY_ECHO"] = True # Debug
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Classroom(db.Model):
+	__tablename__ = "classroom"
+	code = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(16), nullable = False)
+
+	def __repr__(self):
+		return f"Classroom(code: {self.code}, name: {self.name})"
+
+class SchoolHour(db.Model):
+	__tablename__ = "school_hour"
+	id = db.Column(db.Integer, primary_key = True)
+	classroom_id = db.Column(db.Integer, nullable = False)
+	start_time = db.Column(db.Time, nullable = False)
+	end_time = db.Column(db.Time, nullable = False)
+	teacher = db.Column(db.String(32))
+	day = db.Column(db.Date, nullable = False)
+	school_subject = db.Column(db.String(32))
+
+	def __repr__(self):
+		return f"SchoolHour(id: {self.id}, classroom_id: {self.classroom_id}, start_time: {self.start_time}, end_time: {self.endtime}, teacher: {self.teacher}, day: {self.day}, school_subject: {self.school_subject})"
+
+db.create_all()
 
 @app.route("/")
 def index():
