76283b201c0c0d8cf3ddf97b981c99b30a9ca304
PiCas19 <pierpaolo.casati@samtrevano.ch>
2020-03-13 14:20:53 +0100

    Update script.js

--- Materiale/GPS/script.js
+++ Materiale/GPS/script.js
@@ -1,5 +1,5 @@
 function load(){
-    setInterval(getGeoLocation, 10);
+    setInterval(getGeoLocation, 100);
 }
 
 
@@ -7,29 +7,31 @@ function load(){
 
 function getGeoLocation(){
     if(navigator.geolocation){
-        navigator.geolocation.watchPosition(onSuccess, onError);
+      navigator.geolocation.watchPosition(onSuccess, onError);
     }
-    else{
-        console.log('Not supported');
+    else
+    {
+      console.log('Not supported');
     }
+    console.log("Ciao");
 }
 
 function onSuccess(position){
     const{latitude, longitude} = position.coords;
 
 
-    document.getElementById('demo').innerHTML =  
-    "Latitudine: " + latitude + "</br>" + 
-    "Longitudine: " + longitude + "</br>" + 
-    "Timestamp: " + position.timestamp + "</br>" + 
-    "Distanza: " + calculateDistance(latitude, longitude, 46.845737,  9.231766);
+    document.getElementById('demo').innerHTML =
+    "Latitudine: " + latitude + "</br>" +
+    "Longitudine: " + longitude + "</br>" +
+    "Timestamp: " + position.timestamp + "</br>" +
+    "Distanza: " + calculateDistance(46.033328, 8.967561, latitude,  longitude);
 
     console.log(latitude);
     console.log(longitude)
 }
 
 function onError(error){
-    console.log(error);   
+    console.log(error);
 }
 
 
@@ -46,8 +48,8 @@ function calculateDistance(lat, long, start_lat, start_long) {
     var disLat = (lat*Math.PI*earthRadius)/180; // Vertical distance
     var disLon = (lon*Math.PI*earthRadius)/180; // Horizontal distance
 
-    var ret = Math.pow(disLat, 2) + Math.pow(disLon, 2); 
+    var ret = Math.pow(disLat, 2) + Math.pow(disLon, 2);
     ret = Math.sqrt(ret); // Total distance (calculated by Pythagore: a^2 + b^2 = c^2)
 
     return ret;
-} 
+}
