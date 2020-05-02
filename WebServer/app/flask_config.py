eff49d45659c555a67e05c9430e1dea4796b496b
lorenzoap <lorenzo.piazza@samtrevano.ch>
2020-05-02 23:29:03 +0200

    Rimosso riferimento al server MySQL rimosso in precedenza dalla configurazione di Flask

--- WebServer/app/flask_config.py
+++ WebServer/app/flask_config.py
@@ -2,13 +2,12 @@ import os
 
 class FlaskConfigBase:
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_DATABASE_URI = "sqlite:///classroom.db"
 
 class FlaskConfigDebug(FlaskConfigBase):
-	SQLALCHEMY_DATABASE_URI = "sqlite:///classroom.db"
 	SQLALCHEMY_ECHO = True
 
 class FlaskConfigRelease(FlaskConfigBase):
-	SQLALCHEMY_DATABASE_URI = "mysql+pymysql://query:Punk42h2AsaCbzs@db:3306/classroom_timetables"
 	SQLALCHEMY_ECHO = False
 
 def get_flask_config():
