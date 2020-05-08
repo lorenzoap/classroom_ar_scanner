1b3472caa80ae96af3f58ac12e758876ed730642
lorenzoap <lorenzo.piazza@samtrevano.ch>
2020-05-08 02:09:43 +0200

    Aggiunta documentazione

--- WebServer/app/flask_config.py
+++ WebServer/app/flask_config.py
@@ -1,17 +1,21 @@
 import os
 
 class FlaskConfigBase:
+	"""Contiene la configurazione base per Flask"""
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SQLALCHEMY_DATABASE_URI = "sqlite:///classroom.db"
 	SECRET_KEY = "eAWYiIPDcjIbYxrr1qfn"
 
 class FlaskConfigDebug(FlaskConfigBase):
+	"""Contiene la configurazione di debug per Flask"""
 	SQLALCHEMY_ECHO = True
 
 class FlaskConfigRelease(FlaskConfigBase):
+	"""Contiene la configurazione di produzione per Flask"""
 	SQLALCHEMY_ECHO = False
 
 def get_flask_config():
+	"""Restituisce la configurazione di Flask da usare in base alla variabile d'ambiente "DEBUG_MODE"."""
 	debug_mode = os.environ.get("DEBUG_MODE")
 
 	if debug_mode in ["yes", "on", "true"]:
