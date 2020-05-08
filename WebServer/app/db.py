6b1fc6fe24483b8964e4c1db22bbb1d96a3d2f57
lorenzoap <lorenzo.piazza@samtrevano.ch>
2020-05-08 14:58:16 +0200

    Aggiunta relazione tra le tabelle Classroom e SchoolHour

--- WebServer/app/db.py
+++ WebServer/app/db.py
@@ -12,6 +12,7 @@ class Classroom(db.Model):
 	__tablename__ = "classroom"
 	code = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(16), nullable = False, unique = True)
+	school_hours = db.relationship("SchoolHour", backref = "classroom", cascade = "all, delete-orphan")
 
 	def __repr__(self):
 		return f"Classroom(code: {self.code}, name: {self.name})"
@@ -21,12 +22,12 @@ class SchoolHour(db.Model):
 
 	__tablename__ = "school_hour"
 	id = db.Column(db.Integer, primary_key = True)
-	classroom_id = db.Column(db.Integer, nullable = False)
 	start_time = db.Column(db.Time, nullable = False)
 	end_time = db.Column(db.Time, nullable = False)
 	teacher = db.Column(db.String(32))
 	day = db.Column(db.Date, nullable = False)
 	school_subject = db.Column(db.String(32))
+	classroom_id = db.Column(db.Integer, db.ForeignKey("classroom.code"), nullable = False)
 
 	def __repr__(self):
 		return f"SchoolHour(id: {self.id}, classroom_id: {self.classroom_id}, start_time: {self.start_time}, end_time: {self.end_time}, teacher: {self.teacher}, day: {self.day}, school_subject: {self.school_subject})"
